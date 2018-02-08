293d4fd3d7766783971f455253958494d8b839ef
scottbarnesg <scottgbarnes@gwu.edu>
2018-02-08 13:46:31 -0500

    Add Bash Script

---
+++ map_sim/run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#SBATCH -o mapsim_%j.out
+#SBATCH -e mapsim_%J.err
+
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=scottgbarnes@gwu.edu
+
+#SBATCH -N 1
+#SBATCH -p cpu
+
+#SBATCH -D /home/scottgbarnes/map_sim
+#SBATCH -J search_v2_1
+
+#SBATCH -t 24:00:00
+
+module load anaconda/4.4.6
+python3 run_mapEnv.py
